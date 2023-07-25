fd03a697934d5bf4e866a7bcf6e5c60dbe498dd6
Aaron Ellington <aaron@ellington.io>
2023-07-25 15:11:38 -0400

    initial commit

---
+++ environment/environment.go
@@ -0,0 +1,101 @@
+package environment
+
+import (
+	"errors"
+	"io"
+	"os"
+	"reflect"
+	"strconv"
+	"strings"
+)
+
+func New(useExistingValuesFromSystem bool) *Environment {
+	env := &Environment{
+		vars: map[string]string{},
+	}
+
+	if useExistingValuesFromSystem {
+		for _, e := range os.Environ() {
+			pair := strings.SplitN(e, "=", 2)
+			env.vars[pair[0]] = pair[1]
+		}
+	}
+
+	return env
+}
+
+type Environment struct {
+	vars map[string]string
+}
+
+func (env Environment) Decode(target any) error {
+	targetInstance := reflect.ValueOf(target)
+	if targetInstance.Kind() != reflect.Pointer {
+		return errors.New("must provider pointer")
+	}
+
+	targetInstance = targetInstance.Elem()
+
+	for i := 0; i < targetInstance.NumField(); i++ {
+		fieldInstance := targetInstance.Field(i)
+		fieldDefinition := targetInstance.Type().Field(i)
+
+		tag := fieldDefinition.Tag.Get("env")
+		if tag == "" {
+			continue
+		}
+
+		valueFromEnv, ok := env.vars[tag]
+		if !ok {
+			continue
+		}
+
+		switch fieldDefinition.Type.Kind() {
+		case reflect.String:
+			fieldInstance.SetString(valueFromEnv)
+		case reflect.Bool:
+			convertedValue, err := strconv.ParseBool(valueFromEnv)
+			if err != nil {
+				return err
+			}
+
+			fieldInstance.SetBool(convertedValue)
+
+		case reflect.Int:
+			convertedValue, err := strconv.Atoi(valueFromEnv)
+			if err != nil {
+				return err
+			}
+
+			fieldInstance.SetInt(int64(convertedValue))
+		}
+
+	}
+
+	return nil
+}
+
+func (env Environment) Encode(reader io.Reader) error {
+	bodyBytes, err := io.ReadAll(reader)
+	if err != nil {
+		return err
+	}
+
+	for _, line := range strings.Split(string(bodyBytes), "\n") {
+		parts := strings.SplitN(line, "=", 2)
+		key := parts[0]
+		value := parts[1]
+
+		// Skip the env variable is already set
+		if _, alreadySet := env.vars[key]; alreadySet {
+			continue
+		}
+
+		// line = strings.Trim(line, "\"'")
+
+		// Set the value
+		env.vars[key] = value
+	}
+
+	return nil
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/aaronellington/environment-go
+
+go 1.20
